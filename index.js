95d8e647ace635f04d97c833f365e8f4650dd1dc
Nir Mekin <nir.mekin@gmail.com>
2017-06-25 19:43:01 +0300

    fix header

--- index.js
+++ index.js
@@ -12,15 +12,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //return API of WS
-app.get('/',(req,res)=>{
-    "use strict";
-    res.sendFile(__dirname + "/API_Genres/");
-})
+// app.get('/',(req,res)=>{
+//     "use strict";
+//     res.sendFile(__dirname + "/API_Genres/");
+// })
+
+app.use('/', express.static('./API_Genres/'));//for API
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.set("Content-Type", "application/json");
+    next();
+});
 
 //return all data of cat_genres.json
 app.get('/getAllGenres',(req,res)=> {
     "use strict";
-    res.setHeader('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Origin','*');
     try {
         genres.getAllGenres().then((result) => {
             res.json(result);
